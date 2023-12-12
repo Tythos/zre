3c0db10a9223452f3cd7c0687e5513cbf41bff00
Brian Kirkpatrick <code@tythos.net>
2023-12-11 19:49:10 -0800

    ready to try a package build?

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # zre
+
 Regular expression package for Zig targeting package installation
+
+There are a number of stale regex implementations for Zig, none of which are compatible with v0.12 out of the box or supported by Zig packaging (a la .ZON references). Inspired by two in particular:
+
+https://github.com/tiehuis/zig-regex
+
+https://github.com/alexnask/ctregex.zig
+
+## Using
+
+Top-level exports include:
+
+* `Encoding`
+
+* `MatchOptions`
+
+* `MatchResult`
+
+* `match()`
+
+* `search()`
+
+For the time being, basic examples for these symbols are best drawn from `tests.zig`.
---
+++ build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // define static library
+    const lib = b.addStaticLibrary(.{
+        .name = "zre",
+        .root_source_file = .{ .path = "zre.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
+
+    // define library tests
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "tests.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(main_tests);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
